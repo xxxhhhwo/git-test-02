22b49058040de7c5668f632fd71477e4ed35d289
songshaoqiu <ssq@qq.com>
2021-11-29 11:20:25 +0800

    master commit02

--- src/main/java/com/atguigu/test/Demo01.java
+++ src/main/java/com/atguigu/test/Demo01.java
@@ -5,5 +5,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello git---by master");
         System.out.println("使用master 测试");
+        System.out.println("即将让master跟hot-fix第一次合并");
     }
 }
