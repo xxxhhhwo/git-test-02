f210b51aa2aa751490a8d8d549e10a8167c6c099
songshaoqiu <ssq@qq.com>
2021-11-29 11:15:46 +0800

    hot-fix commit01

--- src/main/java/com/atguigu/test/Demo01.java
+++ src/main/java/com/atguigu/test/Demo01.java
@@ -5,5 +5,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello git---by master");
         System.out.println("使用master 测试");
+        System.out.println("使用hot-fix 写的第一句话");
     }
 }
